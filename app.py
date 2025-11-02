bfcf26703bab2c0236ba274bc63cb6a78dabec7d
Samyogjungkarki <Samyogjk@gmail.com>
2025-11-02 15:15:24 +0545

    edit contact

--- app.py
+++ app.py
@@ -11,3 +11,19 @@ if __name__ == '__main__':
 def home():
     contacts = ContactService.get_all_contacts()
     return render_template('index.html', contacts=contacts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        
+        success, message = ContactService.create_contact(name, phone)
+        
+        if success:
+            flash(message, 'success')
+            return redirect('/')
+        else:
+            flash(message, 'error')
+    
+    return render_template('add.html')
