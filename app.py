877fb79d5ce58cf449300294d1e322400827fb4c
Samyogjungkarki <Samyogjk@gmail.com>
2025-11-02 14:08:26 +0545

    home route

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ app.secret_key = 'abc123'
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
+
+@app.route('/')
+def home():
+    contacts = ContactService.get_all_contacts()
+    return render_template('index.html', contacts=contacts)
